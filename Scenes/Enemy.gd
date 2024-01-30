3de9e088c94de605a74958d1c4411d2f5c01bed2
bubby8jj <118482946+bubby8jj@users.noreply.github.com>
2024-01-30 13:06:50 -0600

    started making enemy

---
+++ Scenes/Enemy.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+const speed = 250
+
+@export var player: Node2D
+@onready var nav_agent:=$NavigationAgent2D as NavigationAgent2D
+
+func _physics_process(_delta: float) -> void:
+	var direction = to_local(nav_agent.get_next_path_position()).normalized()
+	velocity = direction * speed
+	move_and_slide()
+	
+func makepath() -> void:
+	nav_agent.target_position = player.global_positionk
+
+
+func _on_timer_timeout():
+	pass # Replace with function body.
