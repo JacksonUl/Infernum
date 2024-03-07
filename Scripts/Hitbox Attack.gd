0545508c0c9629694e083ea90566558186f885ce
Teddy-7 <118483331+Teddy-7@users.noreply.github.com>
2024-03-07 14:44:06 -0600

    Update Hitbox Attack.gd

--- Scripts/Hitbox Attack.gd
+++ Scripts/Hitbox Attack.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
 @onready var swordHitbox = $Area2D/CollisionShape2D
+
+var tween
 var swordlength = 30
 var direction
 var rotationAngle
+
+
 signal damage
 
 func _ready():
 	pass
-
+	
 func _process(delta):
 	
 	var col = get_node("Area2D/CollisionShape2D")
@@ -18,8 +22,27 @@ func _process(delta):
 	rotation = rotationAngle
 	swordHitbox.position = direction.normalized() * swordlength
 	
+	if Input.is_action_just_pressed("Attak"):
+		#$AnimationPlayer.play("Shing Shing")
+		if rotation > 90 and rotation < 270:
+			newTween()
+			tween.tween_property($Area2D, "rotation", deg_to_rad(-90), 1)
+			
+		else:
+			newTween()
+			tween.tween_property($Area2D, "rotation", deg_to_rad(90), 1)
+			
+			
+		
+
+func newTween():
+	tween = create_tween()
+	#get_tree().create_timer(desired value)
+	tween.kill()
 
 
 func _on_area_2d_body_entered(body):
 	if Input.is_action_just_pressed("Attak"):
 		emit_signal("damage")
+
+
