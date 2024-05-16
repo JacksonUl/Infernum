2e1bf5cf5e5484f4dc9c03619aeaffb97022bf83
JacksonUl <jeulland26@blakeschool.org>
2024-05-16 14:37:58 -0500

    score counter

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 signal mobPath
 signal enemyDamage(enemytype)
+signal enemykilled
+
+
+var score = 0
 
 var mob_scene = preload("res://Scenes/Enemy.tscn").instantiate()
 var othermob = preload("res://Scenes/enemy2.tscn").instantiate()
@@ -9,11 +13,12 @@ var othermob = preload("res://Scenes/enemy2.tscn").instantiate()
 func _ready():
 	mob_scene.add_to_group("Enemys")
 	othermob.add_to_group("Enemys")
-	mob_scene.position = Vector2(100, 400)
-	othermob.position = Vector2(120, 900)
+	mob_scene.position = Vector2(-500, 300)
+	othermob.position = Vector2(200, 500)
 	add_child(mob_scene)
 	add_child(othermob)
 	emit_signal("mobPath")
+	$ScoreLabel.text = ("Score: " + str(score))
 	
 
 # Press escape to quit the game
@@ -29,4 +34,9 @@ func _on_player_character_death():
 func _on_sword_hitbox_body_entered(body):
 	if body.is_in_group("Enemys"):
 		body.queue_free()
-		
+		emit_signal("enemykilled")
+
+
+func _on_enemykilled():
+	score += 1
+	$ScoreLabel.text = ("Score: " + str(score))
